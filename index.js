bf55114299052d4e44fb92e3283e06ca0318ce89
Akseli Palen <akseli.palen@gmail.com>
2018-05-20 23:19:27 +0300

    Initial main architectural lines.

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 // API v4
 
-exports.geom = require('./lib/geom')
-exports.Space = require('./lib/Space')
-exports.SpaceGroup = require('./lib/SpaceGroup')
-exports.SpaceHTML = require('./lib/SpaceHTML')
-exports.SpaceImage = require('./lib/SpaceImage')
-exports.SpacePixel = require('./lib/SpacePixel')
-exports.SpaceView = require('./lib/SpaceView')
-exports.Touchable = require('./lib/Touchable')
-exports.Wheelable = require('./lib/Wheelable')
-// Maybe in future:
-// exports.SpacePoint = require('./lib/SpacePoint')
-// exports.SpaceTransform = require('./lib/SpaceTransform')
+var t = function (el) {
+  var h = new t.SpaceHTML(el)
+  return h
+}
 
-exports.preload = require('loadimages')
-exports.version = require('./lib/version')
+module.exports = t
+
+t.geom = require('./lib/geom')
+t.Space = require('./lib/Space')
+t.SpaceHTML = require('./lib/SpaceHTML')
+t.Touchable = require('./lib/Touchable')
+t.Wheelable = require('./lib/Wheelable')
+// Maybe in future:
+// t.SpacePoint = require('./lib/SpacePoint')
+// t.SpaceTransform = require('./lib/SpaceTransform')
+
+t.version = require('./lib/version')
